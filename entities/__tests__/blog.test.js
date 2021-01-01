3f279ce00afe39f33776469e9a650b2ead2c2138
Tyler Norlund <tnorlund@icloud.com>
2021-01-01 12:56:46 -0800

    🐛 Fixed github action

--- entities/__tests__/blog.test.js
+++ entities/__tests__/blog.test.js
@@ -19,38 +19,40 @@ const invalidBlogs = [
   { numberProjects: `-1` },
 ]
 
-test.each( validBlogs )(
-  'blog valid constructor', 
-  parameter => expect( new Blog( parameter ) ).toEqual( {
-    numberUsers: 0, numberPosts: 0, numberProjects: 0
+describe( `blog object`, () => {
+  test.each( validBlogs )(
+    'valid constructor', 
+    parameter => expect( new Blog( parameter ) ).toEqual( {
+      numberUsers: 0, numberPosts: 0, numberProjects: 0
+    } )
+  )
+  
+  test.each( invalidBlogs )(
+    `invalid constructor`,
+    parameter => expect( () => new Blog( parameter ) ).toThrow()
+  )
+  
+  test( `blog key`, () => {
+    expect( new Blog( {} ).key() ).toEqual( {
+      'PK': { 'S': `#BLOG` },
+      'SK': { 'S': `#BLOG` }
+    } )
   } )
-)
-
-test.each( invalidBlogs )(
-  `blog invalid constructor`,
-  parameter => expect( () => new Blog( parameter ) ).toThrow()
-)
-
-test( `blog key`, () => {
-  expect( new Blog( {} ).key() ).toEqual( {
-    'PK': { 'S': `#BLOG` },
-    'SK': { 'S': `#BLOG` }
+  
+  test( `toItem`, () => {
+    const blog = new Blog( {} )
+    expect( blog.toItem() ).toStrictEqual( {
+      'PK': { 'S': `#BLOG` },
+      'SK': { 'S': `#BLOG` },
+      'Type': { 'S': `blog` },
+      'NumberUsers': { 'N': `0` },
+      'NumberPosts': { 'N': `0` },
+      'NumberProjects': { 'N': `0` }
+    } )
   } )
-} )
-
-test( `blog toItem`, () => {
-  const blog = new Blog( {} )
-  expect( blog.toItem() ).toStrictEqual( {
-    'PK': { 'S': `#BLOG` },
-    'SK': { 'S': `#BLOG` },
-    'Type': { 'S': `blog` },
-    'NumberUsers': { 'N': `0` },
-    'NumberPosts': { 'N': `0` },
-    'NumberProjects': { 'N': `0` }
+  
+  test( `blogFromItem`, () => {
+    const blog = new Blog( {} )
+    expect( blogFromItem( blog.toItem() ) ).toStrictEqual( blog )
   } )
-} )
-
-test( `blog blogFromItem`, () => {
-  const blog = new Blog( {} )
-  expect( blogFromItem( blog.toItem() ) ).toStrictEqual( blog )
 } )
